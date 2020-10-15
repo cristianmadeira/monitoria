8b394fd7373537b32f0e70b3d75e7f8fa4c373e9
Cristian Madeira de Souza Pereira <cristianmadeira1106@gmail.com>
2020-10-15 11:53:32 -0300

    Create Filme Class in model with test

---
+++ locadorascore/src/main/java/br/cefet/rj/mg/bsi/locadorascore/model/Filme.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefet.rj.mg.bsi.locadorascore.model;
+
+/**
+ *
+ * @author cristian
+ */
+public class Filme {
+    
+    private int id;
+    private String sinopse;
+    private String titulo;
+    private String duracao;
+    private String genero;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(String duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    
+    
+    
+}
---
+++ locadorascore/src/test/java/br/cefet/rj/mg/bsi/locadorascore/model/FilmeTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefet.rj.mg.bsi.locadorascore.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author cristian
+ */
+public class FilmeTest {
+    
+    public FilmeTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testIfClassExists() {
+      Filme filme = new Filme();
+        assertNotNull(filme);
+    }
+    
+}
