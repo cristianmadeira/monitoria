e1a732f542246096f4e0116c4fe0927885284018
Cristian Madeira de Souza Pereira <cristianmadeira1106@gmail.com>
2020-10-15 14:28:13 -0300

    Add DAO and DAO Exception

---
+++ locadorascore/src/main/java/br/cefet/rj/mg/bsi/locadorascore/dao/DAO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefet.rj.mg.bsi.locadorascore.dao;
+
+import br.cefet.rj.mg.bsi.locadoracore.exception.DAOException;
+import java.util.List;
+
+
+/**
+ *
+ * @author cristian
+ */
+public interface DAO {
+    
+    public boolean save() throws DAOException;
+    public boolean update() throws DAOException;
+    public boolean delete(int id) throws DAOException;
+    public List findById(int id) throws DAOException;
+    
+}
